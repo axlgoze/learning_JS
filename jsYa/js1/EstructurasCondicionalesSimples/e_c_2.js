e53dab637de6e50641b4e2381af1d6ff5cb8a83d
axlgoze <rbaxel.f@gmail.com>
2024-06-24 21:00:24 -0600

    15.1)JSYA:conditionalStructures2

---
+++ jsYa/js1/EstructurasCondicionalesSimples/e_c_2.js
@@ -0,0 +1,11 @@
+// declaration of variables
+let key1, key2;
+
+key1 = parseInt(prompt("Ingresa la primera clave: "));
+key2 = parseInt(prompt("Ingresa la segunda clave: "));
+
+if(key1 == key2){
+    alert("las claves son iguales");
+}else{
+    alert("Las claves no son iguales");
+}
