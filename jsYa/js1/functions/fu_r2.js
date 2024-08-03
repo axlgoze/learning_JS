388188fd5e3ee3765b120600ae04f5365221db95
Brayan Axel Reyes Flores <brayanaxel.reyes.contractor@bbva.com>
2024-08-02 19:47:39 -0600

    26)feat(fu_r2 fu_r3.js.js):refactor return exercises

--- jsYa/js1/functions/fu_r2.js
+++ jsYa/js1/functions/fu_r2.js
@@ -7,15 +7,15 @@ num2=parseInt(prompt("Enter the second number"));
 num3=parseInt(prompt("Enter the third number"));
 
 function maxValue(num1,num2,num3){
-    if(num1<num2 && num1<num3){
-        return document.write("the lower is: "+num1);
+    if(num1>num2 && num1>num3){
+        return num1;
     }else{
-        if(num2 < num3){
-            return document.write("The lower is: "+num2);
+        if(num2 > num3){
+            return num2;
         }else{
-            return document.write("The lower is: "+num3);
+            return num3;
         }
     }
 }
-
-maxValue(num1,num2,num3);
+let result=maxValue(num1,num2,num3);
+document.write("The higher value is: "+result);
--- jsYa/js1/functions/fu_r3.js
+++ jsYa/js1/functions/fu_r3.js
@@ -1,13 +1,10 @@
 // Elaborar una función a la cual le envíe el valor del lado de un cuadrado y me retorne su perímetro.
 
-let side, perimeter;
-
-side=parseInt(prompt("Enter the side of a square"));
+let side=parseInt(prompt("Enter the side of a square"));
 
 function calculatePerimeter(side){
-    perimeter= side*4;
+    let perimeter= side*4;
     return perimeter;
 }
 
-
-document.write(calculatePerimeter(side));
+document.write("The perimeter is: "+calculatePerimeter(side));
