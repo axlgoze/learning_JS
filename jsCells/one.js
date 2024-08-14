98a5ca114f059c7fe8b672dbc81d671bb6cc1d50
Brayan Axel Reyes Flores <brayanaxel.reyes.contractor@bbva.com>
2024-08-13 23:28:14 -0600

    A)feat(jsCells):add folder to study cells samples[JIRA-12345]

---
+++ jsCells/one.js
@@ -0,0 +1,13 @@
+let names = "Carlos/José/Sandra/Ana/Rosa";
+
+let aNames = names.split();
+
+document.write(aNames);
+
+console.log(typeof(names));
+
+console.log(typeof(aNames));
+
+console.log(aNames.length);
+
+console.log(names);
---
+++ jsCells/two.js
@@ -0,0 +1,10 @@
+// what is the street value?
+//employee object with two properties
+const employee = {
+    firstName: 'Axl',
+    age: 30
+}
+
+const street = employee.address?.street;
+
+console.log(street);
