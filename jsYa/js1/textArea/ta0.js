b90b1f751884b425d6edaf883501ddd2aa2560cf
Brayan Axel Reyes Flores <brayanaxel.reyes.contractor@bbva.com>
2024-10-08 19:39:34 -0600

    34)feat(jsYa/js1/textArea/*):textArea excercise

---
+++ jsYa/js1/textArea/ta0.js
@@ -0,0 +1,6 @@
+// Confeccionar una página de visitas a un sitio, solicitar ingresar el nombre de una persona, su mail y los comentarios (TEXTAREA). Mostrar luego llamando a la función alert los datos ingresados.
+
+function validateComment(){
+    let opinion = document.getElementById('textArea1').value;
+    alert(opinion);
+}
