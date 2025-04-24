c2ef8362c21d4d3af2786eaa82f1e52ac5b0ed61
Brayan Axel Reyes Flores <brayanaxel.reyes.contractor@bbva.com>
2025-04-23 19:28:25 -0600

    28.1)feat(jsYa/js1/Array/):add scattered array excercises[JIRA-1234]

--- jsYa/js1/Array/arr5.js
+++ jsYa/js1/Array/arr5.js
@@ -1,23 +1,32 @@
-// Crear un vector vacío. Mediante una estructura repetitiva solicitar la carga de elementos por teclado hasta que se ingrese el cero. No almacenar dicho valor en el vector. Luego sumar todas las componentes del vector, mostrar dicha suma y el tamaño del vector.
+// Crear un vector vacío. 
+// Mediante una estructura repetitiva solicitar la carga de elementos por teclado hasta que se ingrese el cero. No almacenar dicho valor en el vector. 
+// Luego sumar todas las componentes del vector, 
+// mostrar dicha suma
+// mostrar tamaño del vector.
+
+
+// empty array
+let emptyArr = new Array();
 
 let input;
-let emptyArr = new Array();
 let sum = 0;
+let i=0;
 
+// repetitive structure
 do{
-    input = parseInt("Enter a number: ");
-    
-    if(input!=0){
-        for(let f=0; f<emptyArr.length; f++){
-            emptyArr[f] = input;
-        }
-    }
-}while(input!=0){
+    input = parseInt(prompt("Enter a number: "));
 
+    // add input to the empty array
+    if(input!=0){
+        emptyArr[i]=input;
+        i++;
+    }
+}while(input!=0);
+    //sum elements of vector
     for(let f=0; f<emptyArr.length; f++){
         sum = sum + emptyArr[f];
     }
-
-    console.log('emptyArr', emptyArr);
-
-}
+    //show vector size
+    console.log('Array: ' +emptyArr + ' length:' + emptyArr.length);
+    //show sum
+    console.log('sum', sum);
